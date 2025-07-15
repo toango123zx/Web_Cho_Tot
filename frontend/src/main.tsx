99b82d6793bcff67d7e4c83b70fef222b15b661a
vvm1004 <vovanminhv23@gmail.com>
2025-07-15 09:02:31 +0700

    feat: add Layout component and integrate with router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,30 +4,37 @@ import './index.css'
 import LoginPage from './pages/LoginPage.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage.tsx'
-import { AppProvider } from '@/components/context/app.context.tsx'
-import ProtectedRoute from '@/components/auth/index.tsx'
+// import { AppProvider } from '@/components/context/app.context.tsx'
+// import ProtectedRoute from '@/components/auth/index.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
-import { SocketProvider } from './components/context/socket.context.tsx'
+// import { SocketProvider } from './components/context/socket.context.tsx'
 import HomePage from './pages/HomePage.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Layout from './layout.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      // <ProtectedRoute>
-        <HomePage />
-      // </ProtectedRoute>
+      <Layout />
     ),
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ]
   },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
+
 ])
 const queryClient = new QueryClient()
 
@@ -35,12 +42,14 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    {/* <AppProvider> */}
+      {/* <AppProvider> */}
       {/* <SocketProvider> */}
-        <RouterProvider router={router} />
-        <Toaster />
+      <RouterProvider router={router} />
+      <Toaster />
+      <ReactQueryDevtools initialIsOpen={false} />
+
       {/* </SocketProvider> */}
-    {/* </AppProvider> */}
+      {/* </AppProvider> */}
     </QueryClientProvider>
   </StrictMode>,
 )
